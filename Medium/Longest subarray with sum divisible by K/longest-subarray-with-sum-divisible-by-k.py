26d81cbc95224c42c12c8129ec577abdf082f334
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-10 22:30:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest subarray with sum divisible by K/longest-subarray-with-sum-divisible-by-k.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longSubarrWthSumDivByK(self, nums, length, k):
+        remainder_index_map, current_remainder_sum, max_length = {0: -1}, 0, 0
+
+        for i in range(length):
+            current_remainder_sum = (current_remainder_sum + nums[i]) % k
+            max_length = max(max_length, i - remainder_index_map.get(current_remainder_sum, i))
+
+            remainder_index_map.setdefault(current_remainder_sum, i)
+
+        return max_length
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+	for _ in range(0,int(input())):
+		n, K = map(int ,input().split())
+		arr = list(map(int,input().strip().split()))
+		ob = Solution()
+		res = ob.longSubarrWthSumDivByK(arr, n, K)
+		print(res)
+
+
+
+
+# } Driver Code Ends
