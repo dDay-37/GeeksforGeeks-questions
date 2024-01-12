7879d0d7daf5d318d667b68d42b235759456ad38
Uday <91021882+dDay-37@users.noreply.github.com>
2024-01-12 23:25:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class Solution:
+    def modifyQueue(self, queue, k):
+        temp_queue = deque(queue[:k][::-1])
+        del queue[:k]
+        queue[:0] = temp_queue
+        return queue
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+
+        queue = [] # our queue to be used
+        for i in range(n):
+            queue.append(a[i]) # enqueue elements of array in our queue
+        
+        ob = Solution()
+        print(*ob.modifyQueue(queue,k))
+        
+# } Driver Code Ends
