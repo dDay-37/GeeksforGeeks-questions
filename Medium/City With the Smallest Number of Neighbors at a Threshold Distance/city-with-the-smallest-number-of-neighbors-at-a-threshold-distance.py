5e907bf30e52f12cafc3dd1417ed08d5e080e186
Uday <91021882+dDay-37@users.noreply.github.com>
2024-03-28 17:17:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -3,7 +3,7 @@
 from typing import List
 import heapq
 class Solution:
-    def findCity(self, n : int, m : int, edges : List[List[int]], d : int) -> int:
+    def findCity(self, n : int, m : int, edges : List[List[int]], thres : int) -> int:
         adj=[[] for _ in range(n)]
         for u,v,w in edges:
             adj[u].append((v,w))
@@ -25,34 +25,13 @@ class Solution:
         mn=n
         ans=-1
         for i in range(n):
-            c=sum(1 for h in dijk(i) if h<=d)
+            c=sum(1 for h in dijk(i) if h<=thres)
             if c<=mn:
                 mn=c
                 ans=i
         return ans
         
         
-"""
-City 0 -> [City 1] 
-City 1 -> [City 0, City 4] 
-City 2 -> [City 3, City 4] 
-City 3 -> [City 2, City 4]
-City 4 -> [City 1, City 2, City 3]
-# mat=[[float('inf')]*n for _ in range(n)]
-        # for x in range(n):
-        #     mat[x][x]=0
-        # for u,v,w in edges:
-        #     mat[u][v]=w
-        #     mat[v][u]=w
-        # for via in range(n):
-        #     for i in range(n):
-        #         for j in range(n):
-        #             mat[i][j]=min(mat[i][j],mat[i][via]+mat[via][j])
-        # for _ in mat:
-        #     print(_)
-        
-"""
-
 
 #{ 
  # Driver Code Starts
