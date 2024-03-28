1ad62fc7d4fd7968153cba512f9bf24517be0089
Uday <91021882+dDay-37@users.noreply.github.com>
2024-03-28 11:59:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distance from the Source (Bellman-Ford Algorithm)/distance-from-the-source-bellman-ford-algorithm.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    # Function to construct and return cost of MST for a graph
+    # represented using adjacency matrix representation
+    '''
+    V: nodes in graph
+    edges: adjacency list for the graph
+    S: Source
+    '''
+    def bellman_ford(self, V, edges, S):
+        #code here
+        dist=[10**8]*V
+        dist[S]=0
+        for _ in range(V-1):
+            for u,v,d in edges:
+                if dist[u] != 10**8 and dist[u]+d<dist[v]:
+                    dist[v]=dist[u]+d
+        for u,v,d in edges:
+            if dist[u] != 10**8 and dist[u]+d<dist[v]:
+                return [-1]
+        return dist
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        V,E = map(int,input().strip().split())
+        edges = []
+        for i in range(E):
+            u,v,w = map(int,input().strip().split())
+            edges.append([u,v,w])
+        S=int(input())
+        ob = Solution()
+        
+        res = ob.bellman_ford(V,edges,S)
+        for i in res:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
