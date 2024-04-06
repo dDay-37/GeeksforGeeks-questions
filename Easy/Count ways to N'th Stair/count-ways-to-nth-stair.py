2ddcbfedfc394f622f6245a25ed1ec7f4fb78205
Uday <91021882+dDay-37@users.noreply.github.com>
2024-04-06 21:58:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count ways to N'th Stair/count-ways-to-nth-stair.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+
+class Solution:
+
+    def countWays(self, n):
+        return n//2 +1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        ob = Solution()
+        print(ob.countWays(n))
+
+# } Driver Code Ends
