884d420f54e84a132d86c11f81375b8f2c041b3d
Uday <91021882+dDay-37@users.noreply.github.com>
2024-04-10 12:58:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Party of Couples/party-of-couples.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def findSingle(self, n, arr):
+        r=arr[0]
+        for i in arr[1:]:
+            r^=i
+        return r
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        arr = input().split()
+        for itr in range(N):
+            arr[itr] = int(arr[itr])
+        
+        ob = Solution()
+        print(ob.findSingle(N, arr))
+
+# } Driver Code Ends
