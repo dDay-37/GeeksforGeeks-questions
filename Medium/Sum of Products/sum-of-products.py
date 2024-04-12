f47c3968de9d131883be9f3b5508ccd4ff887689
Uday <91021882+dDay-37@users.noreply.github.com>
2024-04-12 23:31:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Products/sum-of-products.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+class Solution:
+    def pairAndSum(self, n, arr):
+        # Code here
+        result = 0
+        for i in range(32):
+            c = 0
+            
+            for j in arr:
+                if (j >> i) & 1:
+                    c += 1
+            
+            if c > 1:
+                result += (2 ** i) * ((c - 1) * c) // 2
+        
+        return result
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N=int(input())
+        Arr=list(map(int,input().strip().split(' ')))
+        ob=Solution()
+        print(ob.pairAndSum(N,Arr))
+# } Driver Code Ends
