038506944c3b3d45fbac68db494fc57be835d31f
Uday <91021882+dDay-37@users.noreply.github.com>
2024-04-15 22:59:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count the elements/count-the-elements.py
@@ -0,0 +1,26 @@
+from bisect import bisect_right
+class Solution:
+    def countElements(self, a, b, n, query, q):
+        b.sort()
+        return [bisect_right(b, a[q]) for q in query]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int(input())
+for _ in range(0, t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    q = int(input())
+    query = []
+    ob = Solution()
+    for i in range(q):
+        query.append(int(input()))
+    ans = ob.countElements(a, b, n, query, q)
+    for i in range(q):
+        print(ans[i])
+
+# } Driver Code Ends
