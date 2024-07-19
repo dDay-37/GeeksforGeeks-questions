2e72fb5b1b622aec54c690785087d7233fc0d954
Uday <91021882+dDay-37@users.noreply.github.com>
2024-07-19 20:35:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Smaller elements/count-smaller-elements.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def mergeSort(self, enum):
+        half = len(enum) // 2
+        if half:
+            left, right = self.mergeSort(enum[:half]), self.mergeSort(enum[half:])
+            for i in range(len(enum) - 1, -1, -1):
+                if not right or left and left[-1][1] > right[-1][1]:
+                    self.smaller[left[-1][0]] += len(right)
+                    enum[i] = left.pop()
+                else:
+                    enum[i] = right.pop()
+        return enum
+
+    def constructLowerArray(self,arr):
+        self.smaller = [0] * len(arr)
+        self.mergeSort(list(enumerate(arr)))
+        return self.smaller
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.constructLowerArray(arr)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
