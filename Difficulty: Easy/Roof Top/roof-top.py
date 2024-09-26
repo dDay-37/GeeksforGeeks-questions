e96e9b288c34d0b510cb6e36245d1fc4c5086841
Uday <91021882+dDay-37@users.noreply.github.com>
2024-09-26 23:51:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roof Top/roof-top.py
@@ -0,0 +1,38 @@
+class Solution:
+    
+    def maxStep(self, arr):
+        n = len(arr)
+        cnt = 0
+        maxi = 0
+        for i in range(n - 1):
+            if arr[i] < arr[i + 1]:
+                cnt += 1
+            else:
+                maxi = max(maxi, cnt)
+                cnt = 0
+        maxi = max(maxi, cnt)
+        return maxi
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.maxStep(arr))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
