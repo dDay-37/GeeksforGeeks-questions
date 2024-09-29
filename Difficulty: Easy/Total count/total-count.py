4c5d533c53fc50c6edd6e239965f532ad3de7841
Uday <91021882+dDay-37@users.noreply.github.com>
2024-09-29 19:10:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.py
@@ -0,0 +1,33 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def totalCount(self, k, arr):
+        c = 0
+        for i in arr:
+            if i % k == 0:
+                c = c + (i // k)
+            else:
+                c = c + (i // k) + 1
+        return c        # code here
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.totalCount(k,arr)
+        print(res)
+        t -= 1
+
+
+# } Driver Code Ends
