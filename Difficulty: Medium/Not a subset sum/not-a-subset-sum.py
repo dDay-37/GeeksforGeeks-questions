332168faf0ab494f8fc49821600f7c988c5c5cc8
Uday <91021882+dDay-37@users.noreply.github.com>
2024-10-05 21:02:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findSmallest(self, arr):
+        num = 1
+        for i in arr:
+            if i > num:
+                return num
+            num += i
+        return num
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.findSmallest(arr)
+        print(ans)
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
