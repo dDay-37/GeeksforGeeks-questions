ecc0ef74fde7fe18ce437a97f7bdcff3584ea146
Uday <91021882+dDay-37@users.noreply.github.com>
2024-11-16 23:31:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int nonZeroIndex = 0; // Pointer for the next position to place a non-zero element
+        // Traverse the array
+        for (int i = 0; i < n; i++) {
+            // If the current element is non-zero, swap it with the element at nonZeroIndex
+            if (arr[i] != 0) {
+                int temp = arr[nonZeroIndex];
+                arr[nonZeroIndex] = arr[i];
+                arr[i] = temp;
+                // Move the nonZeroIndex forward
+                nonZeroIndex++;
+            }
+        }
+    }
+}
